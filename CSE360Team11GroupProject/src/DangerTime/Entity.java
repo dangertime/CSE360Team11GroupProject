1f08fd57acf5d455c091d906413f128be782d294
Chet Martinez-Droeg <chet.droeg@asu.edu>
2016-04-13 01:11:19 -0700

    Added functions to monster and entity class
    Moved functions common to player and monster up to entity. Changed
    parseInfoFile method to return an array list so that all values are set
    in the constructor and not a utility function.

--- CSE360Team11GroupProject/src/DangerTime/Entity.java
+++ CSE360Team11GroupProject/src/DangerTime/Entity.java
@@ -23,4 +23,21 @@ public abstract class Entity
 		return maxHealth;
 	}
 	
+	/**
+	 * Method to determine the current health status of the entity based on the threshold (i.e. 0, meaning dead...)
+	 * @return Returns true if entity has health above 0
+	 */
+	public boolean isAlive(){
+		return currentHealth > 0;
+	}
+	
+	/**
+	 * Reduced the health of the entity by the amount passed in
+	 * @param damageTaken the amount of damage dealt to the entity. 
+	 */
+	public void takeDamage(int damageTaken){
+		currentHealth  -= damageTaken;
+	}
+	
+	
 }
--- CSE360Team11GroupProject/src/DangerTime/Monster.java
+++ CSE360Team11GroupProject/src/DangerTime/Monster.java
@@ -14,7 +14,7 @@ import java.util.StringTokenizer;
 
 public class Monster extends Entity
 {
-	private final boolean DEBUG = true;
+	private final boolean DEBUG = false;
 
 	
 	//constants made for easy modification and game balancing
@@ -22,22 +22,21 @@ public class Monster extends Entity
 	private static final String MONSTER_INFO_FILENAME = "src/monsters.txt";
 	private static final int MAX_DAMAGE = 10;	//number of sides on damage die
 	private static final int HIT_CHANCE = 75;	// if roll on hit die > HIT_CHANCE, monster misses
+	private static final int BOSS_HEALTH_SCALING_FACTOR = 2;
 	private static final int RUN_FIRST_CHANCE = 19;
 	private static final int FIGHT_OR_RUN_DIE_SIDES = 20;  //PROBABILITY TO RUN ON FIRST MOVE: 
 														   // (RUN_FIRST_CHANCE)/ (FIGHT_OR_RUN_DIE_SIDS)
 	
-	/*
-	 * Data from Super Class:
+	/*Data from Super Class:
 	 *  int currentHealth;
 	 *  int maxHealth;
 	 *  String name;
 	 *  Dice hitChance;
 	 *  Dice runChance;
 	 */
-	
+
 	private int points;
 	private Dice damageDie;
-	private Dice fightOrRun;	//dice to determine if monster will stay to fight or run on first move
 	private boolean stayAndFight = true; //stores the value for the monster staying to fight on first move
 	
 	
@@ -48,17 +47,41 @@ public class Monster extends Entity
 	 */
 	public Monster()
 	{
-		damageDie = new Dice(MAX_DAMAGE);
-		this.hitChance = new Dice(HIT_CHANCE);
+		List<String> monsterInfo = new ArrayList<String>();
 		
-		parseInfoFile();
+		monsterInfo = parseInfoFile();
+		
+		//setting attributes of monster
+		this.name = monsterInfo.get(0) + monsterInfo.get(1);
+		this.maxHealth = Integer.parseInt(monsterInfo.get(2));
+		this.points = Integer.parseInt(monsterInfo.get(3));
+		this.damageDie = new Dice(MAX_DAMAGE);
+		this.hitChance = new Dice(HIT_CHANCE);
+		this.runChance = new Dice(FIGHT_OR_RUN_DIE_SIDES);
+		
+		//rolling to see if monster runs away
+		if(runChance.roll() > RUN_FIRST_CHANCE)
+			stayAndFight = false;
+		
+		//If monster is a boss, scale its health by constant
+		if(this.name.contains("Boss"))
+			this.maxHealth *= BOSS_HEALTH_SCALING_FACTOR;
+		
+		//set current health after determining if monster is a boss	
+		this.currentHealth = this.maxHealth;
+
 	}
+	
+	/**
+	 * @return Returns the name of the monster if debug is set to false. If debug is true, returns all attributes of monster. 
+	 */
 	public String toString(){
 		String toPrint = "";
 		
 		if(DEBUG){
 			toPrint += "Monster: " + name + "\n";
 			toPrint += "Max Health: " + maxHealth + "\n";
+			toPrint += "Current Health: " + currentHealth + "\n";
 			toPrint += "Points: " + points + "\n";
 		}
 		else
@@ -67,18 +90,44 @@ public class Monster extends Entity
 		
 		return toPrint;
 	}
+	
+	
+	/**
+	 * Method that is called to deal damage to player
+	 * @return Returns the amount of damage the monster deals to the player
+	 */
+	public int dealDamage(){
+		return damageDie.roll();
+	}
+	
+
+	/**
+	 * True if monsters roll to determine if running before fighting was > threshold 
+	 * @return True if monster is fighting, false if monster runs away on first move. 
+	 */
+	public boolean stayTofight(){
+		return stayAndFight;
+	}
+	
+	
 	/**
 	 * Reads text files to generate name, health, and points.
 	 * Should probably be broken into several functions
+	 * @return Returns and ArrayList containing information read from file about monster.
+	 * 		-Index 0 = Monster Adjective
+	 * 		-Index 1 = Monster Name/Type
+	 * 		-Index 2 = Monsters health
+	 * 		-Index 3 = Monsters point value
 	 */
-	private void parseInfoFile(){
-		List<String> monsterFileRead = new ArrayList<String>();
-		List<String> monsterAdjFileRead = new ArrayList<String>();
-		Random numGen = new Random();
-		String monsterInfo;
-		String fileLine;
-		int sizeOfMonstList;
-		int sizeOfAdjList;
+	private List<String> parseInfoFile(){
+		List<String> monsterFileRead = new ArrayList<String>();	//holds lines from monster.txt
+		List<String> monsterAdjFileRead = new ArrayList<String>();	//holds lines from adj text file
+		List<String> returnParams = new ArrayList<String>();		//list that gets returned
+		Random numGen = new Random();				//RNG for selecting monster and adj
+		String monsterInfo;							//assigned value from random index of monsterFileRead
+		String fileLine;							//string used to store line read from file, added to lists
+		int sizeOfMonstList;						//holds size of monsterFileRead
+		int sizeOfAdjList;							//holds size of monsterAdjFileRead
 		
 		if(DEBUG){ //list files in current directory. used to determine where to put text files
 			File file = new File(".");
@@ -138,20 +187,28 @@ public class Monster extends Entity
 		
 //~~~~~~get random monster and create the name~~~~~~~~
 		sizeOfAdjList = monsterAdjFileRead.size();
-		this.name = monsterAdjFileRead.get(numGen.nextInt(sizeOfAdjList)) + " ";
 		
+		//add adj of name
+		returnParams.add(monsterAdjFileRead.get(numGen.nextInt(sizeOfAdjList)) + " "); 
+		
+		//sets monsterInfo to string at random index in list
 		sizeOfMonstList = monsterFileRead.size();	
-		monsterInfo = monsterFileRead.get(numGen.nextInt(sizeOfMonstList)); //sets monsterInfo to string at random index in list
+		monsterInfo = monsterFileRead.get(numGen.nextInt(sizeOfMonstList)); 
 		
-		
-		StringTokenizer splitMonst = new StringTokenizer(monsterInfo,",");
+
 
 //~~~~~~~~assign monster attributes~~~~~~~~~~~~		
+		StringTokenizer splitMonst = new StringTokenizer(monsterInfo,",");
 		
-		this.name += splitMonst.nextToken();
-		this.maxHealth = Integer.parseInt(splitMonst.nextToken());
-		this.currentHealth = this.maxHealth;
-		this.points = Integer.parseInt(splitMonst.nextToken());
+		returnParams.add(splitMonst.nextToken());	//add 2nd half of name
+		returnParams.add(splitMonst.nextToken());	//add health
+		returnParams.add(splitMonst.nextToken());	//add points value
+		
+		
+		return returnParams;
+/*		
+		//this.currentHealth = this.maxHealth;
+		//this.points = Integer.parseInt(splitMonst.nextToken());
 		
 		if(DEBUG){ //to check the values of various variable 
 			System.out.println("list.size " + monsterFileRead.size());	
@@ -161,8 +218,9 @@ public class Monster extends Entity
 			System.out.println("max health " + maxHealth);	
 			System.out.println("points " + points);	
 		}
+*/		
 		
-	}
+	}//end parse method
 	
 
 }//end monster class
