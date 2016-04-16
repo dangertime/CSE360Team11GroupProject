06f1ed227cd84f828c1a7f35cae31be25e202322
classicsam <seclasse@asu.edu>
2016-04-15 18:12:05 -0700

    Created junit tests for the event method

--- CSE360Team11GroupProject/src/DangerTime/RoomTest.java
+++ CSE360Team11GroupProject/src/DangerTime/RoomTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RoomTest {
-
+	// making sure that Room objects are properly instantiated 
 	@Test
 	public void testConstructor()
 	{
@@ -14,11 +14,42 @@ public class RoomTest {
 		assertNotNull(room);
 	}
 	
+	// Looking to see if testing mode is enabled
 	@Test
-	public void testFight()
+	public void verifyTestModeEnabled()
 	{
 		Player player = new Player();
 		Room room = new Room(player);
-		room.fight();
+		assertTrue(room.getTesting());
+	}
+	
+	// Checking that a potion event produces the correct message
+	@Test
+	public void testEventPotion()
+	{
+		Player player = new Player();
+		Room room = new Room(player);
+		room.setTestingEventPotion();
+		assertEquals(room.event(), "You found a potion!\n");
+	}
+	
+	// Checking that an empty room event produces the correct message
+	@Test
+	public void testEventEmptyRoom()
+	{
+		Player player = new Player();
+		Room room = new Room(player);
+		room.setTestingEventEmptyRoom();
+		assertEquals(room.event(), "This is an empty room, proceed with caution!\n");
+	}
+	
+	// Checking that a gold event produces the correct message
+	@Test
+	public void testEventGold()
+	{
+		Player player = new Player();
+		Room room = new Room(player);
+		room.setTestingEventGold();
+		assertEquals(room.event(), "You found some gold!\n");
 	}
 }
